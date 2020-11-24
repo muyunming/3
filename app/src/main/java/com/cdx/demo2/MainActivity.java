b2d689fd7c9378e40b6c7916a37c2f2ef198eaea
muyunming <2569236286@qq.com>
2020-11-24 15:17:10 +0800

    test git

--- app/src/main/java/com/cdx/demo2/MainActivity.java
+++ app/src/main/java/com/cdx/demo2/MainActivity.java
@@ -30,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 //        initView();
         initWebView();
+        // test
     }
 
     @SuppressLint({"SetJavaScriptEnabled"})
